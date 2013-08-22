62a20c8cb2e900a0546d368777b04ccf99feb16e
Eduard <kontakt@eduardmayer.de>
2013-08-23 00:05:03 +0200

    fixed timer. yey.

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (e) {
 	var pttbtn = $('.record-right'),
 		recordTime = $('.time'),
 		spcon = $('.speech-container'),
@@ -6,45 +6,60 @@ $(document).ready(function () {
 		min = 0;
 		count = 0,
 		restartCount = function() {
-	if (count !== 0) {
-		return;
-	}
-	count = setInterval(function () {
-		if (sec <= 9 && min == 0) {
-			recordTime.text(min + ':0' + sec++);
-		} else if (sec > 9 && sec < 60 && min == 0) {
-			recordTime.text(min + ':' + sec++);
-		} else if (sec == 60 && min == 0) {
-			sec = 0;
-			min = 1;
-			recordTime.text(min + ':0' + sec++);
-		} else if (sec <= 9 && min == 1) {
-			min = 1;
-			recordTime.text(min + ':0' + sec++);
-		} else if (sec > 9 && sec < 60 && min == 1) {
-			sec = 0;
-			min = 1;
-			recordTime.text(min + 1 + ':' + sec++);
+		if (count !== 0) {
+			return;
 		}
-	}, 1000);};
+		count = setInterval(function () {
+					if (sec <= 9 && min == 0) {
+						recordTime.text(min + ':0' + sec++);
+						return min;}
+					else if (sec > 9 && sec < 60 && min == 0) {
+						recordTime.text(min + ':' + sec++);
+						return min;}
+					else if (sec >= 60) {
+						sec = 0;
+						min = min + 1;
+						recordTime.text(min + ':0' + sec++);
+						return min;}
+					else if (sec <= 9 && min >= 1) {
+						recordTime.text(min + ':0' + sec++);
+						return min;}
+					else if (sec > 9 && sec < 60 && min >= 1) {
+						recordTime.text(min + ':' + sec++);
+						return min;}
+					else if ( sec >= 60 && min >= 1) {
+						sec = 0;
+						min = min + 1;
+						recordTime.text(min + ':' + sec++);
+						return min;}
+		}, 1000);};
 
-	pttbtn.on('mouseenter', function() {
+	pttbtn.on('mouseenter', function(e) {
 		if ($('.swipe-to-cancel').length == 0) {
 			spcon.append('<div class="swipe-to-cancel"></div>');
 		}
-		
 		spcon.css({'right' : "40px"});
 		restartCount();
+
+		$('.swipe-to-cancel').on('mousemove', function(e) {
+			var p = $('.wa-input-container').offset().left,
+				o = $(this);
+
+			console.log(e.clientX - p - 55)
+		});
 	});
 
-	pttbtn.on('mouseleave', function() {
+
+
+
+	// pttbtn.on('mouseleave', function() {
 		// $('.swipe-to-cancel').remove();
 		// spcon.attr("style", "");
 		// recordTime.text('0:00');
 		// clearInterval(count);
 		// count = 0;
 		// time = 0;
-	});
+	// });
 });
 
 // function() {
