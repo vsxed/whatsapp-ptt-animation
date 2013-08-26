6fb1f2670c9953d47ce921f51d055bee2122ae49
Eduard <kontakt@eduardmayer.de>
2013-08-26 20:55:02 +0200

    some logical fixes, nvmd

--- function.js
+++ function.js
@@ -2,14 +2,10 @@ $(document).ready(function (e) {
 	var pttbtn = $('.record-right'),
 		recordTime = $('.time'),
 		spcon = $('.speech-container'),
-		sec = 0,
-		min = 0;
-		count = 0,
+		sec = 0, min = 0, count = 0,
 		restartCount = function() {
-		if (count !== 0) {
-			return;
-		}
-		count = setInterval(function () {
+			if (count !== 0) {return;}
+			count = setInterval(function () {
 					if (sec <= 9 && min == 0) {
 						recordTime.text(min + ':0' + sec++);
 						return min;}
@@ -17,8 +13,7 @@ $(document).ready(function (e) {
 						recordTime.text(min + ':' + sec++);
 						return min;}
 					else if (sec >= 60) {
-						sec = 0;
-						min = min + 1;
+						sec = 0; min = min + 1;
 						recordTime.text(min + ':0' + sec++);
 						return min;}
 					else if (sec <= 9 && min >= 1) {
@@ -28,8 +23,7 @@ $(document).ready(function (e) {
 						recordTime.text(min + ':' + sec++);
 						return min;}
 					else if ( sec >= 60 && min >= 1) {
-						sec = 0;
-						min = min + 1;
+						sec = 0; min = min + 1;
 						recordTime.text(min + ':' + sec++);
 						return min;}
 		}, 1000);};
@@ -43,20 +37,16 @@ $(document).ready(function (e) {
 			axis: "x", 
 			containment: ".speech-container",
 			drag: function() {
-				if ($('.swipe-to-cancel').offset().left - $('.wa-input-container').offset().left == 40) {
+				if ($('.swipe-to-cancel').offset().left - $('.wa-input-container').offset().left === 40) {
 					$(this).draggable('destroy');
 					$(this).remove();
 					spcon.attr("style", "");
 					recordTime.text('0:00');
-					clearInterval(count);
-					count = 0;
-					sec = 0;
-					min = 0;
+					clearInterval(count); count = 0; sec = 0; min = 0;
 				}
-
 			},
 			stop: function() {
-				if ($('.swipe-to-cancel').offset().left - $('.wa-input-container').offset().left > 40) {
+				if ($('.swipe-to-cancel').offset().left - $('.wa-input-container').offset().left >= 40) {
 					$(this).css({'left' : '55px'});
 				}
 			}
